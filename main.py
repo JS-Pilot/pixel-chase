825fdf705391cbe8c80a58cfab0764eadc95d5fd
Johnny <76563520+JS-Pilot@users.noreply.github.com>
2020-12-26 16:54:23 +0200

    Correct first quadrant direction

--- main.py
+++ main.py
@@ -8,32 +8,35 @@ def determineQuadrant():
         current_quadrant = "bottom_left"
     else:
         current_quadrant = "top_left"
-direction = 0
-current_pitch_in_degrees = 0
-current_roll_in_degrees = 0
-current_quadrant = ""
-absolute_roll = 0
-absolute_pitch = 0
-current_quadrant = "top_right"
-main_sprite = game.create_sprite(2, 4)
-goal_sprite = game.create_sprite(2, 0)
-
-def on_forever():
-    global current_roll_in_degrees, current_pitch_in_degrees, absolute_pitch, absolute_roll, direction
-    current_roll_in_degrees = input.rotation(Rotation.ROLL)
-    current_pitch_in_degrees = input.rotation(Rotation.PITCH)
-    determineQuadrant()
-    absolute_pitch = abs(current_pitch_in_degrees)
-    absolute_roll = abs(current_roll_in_degrees)
+def directionForQuadrant():
+    global direction
     direction = Math.atan(absolute_roll / absolute_pitch) * 57.2958
     if current_quadrant == "top_right":
-        direction = direction
+        direction = 90 - direction
     elif current_quadrant == "bottom_right":
         direction = direction + 90
     elif current_quadrant == "bottom_left":
         direction = 270 - direction
     else:
         direction = 270 + direction
+direction = 0
+current_pitch_in_degrees = 0
+current_roll_in_degrees = 0
+current_quadrant = ""
+absolute_pitch = 0
+absolute_roll = 0
+current_quadrant = "top_right"
+main_sprite = game.create_sprite(2, 4)
+goal_sprite = game.create_sprite(0, 0)
+
+def on_forever():
+    global current_roll_in_degrees, current_pitch_in_degrees, absolute_pitch, absolute_roll
+    current_roll_in_degrees = input.rotation(Rotation.ROLL)
+    current_pitch_in_degrees = input.rotation(Rotation.PITCH)
+    determineQuadrant()
+    absolute_pitch = abs(current_pitch_in_degrees)
+    absolute_roll = abs(current_roll_in_degrees)
+    directionForQuadrant()
     main_sprite.set(LedSpriteProperty.DIRECTION, direction)
     main_sprite.move(1)
 basic.forever(on_forever)
@@ -43,5 +46,5 @@ def on_forever2():
     basic.pause(800)
     goal_sprite.delete()
     basic.pause(800)
-    goal_sprite = game.create_sprite(2, 0)
+    goal_sprite = game.create_sprite(0, 0)
 basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-function determineQuadrant () {
+function determineQuadrant() {
+    
     if (current_roll_in_degrees >= 0 && current_pitch_in_degrees <= 0) {
         current_quadrant = "top_right"
     } else if (current_roll_in_degrees >= 0 && current_pitch_in_degrees > 0) {
@@ -8,25 +9,14 @@ function determineQuadrant () {
     } else {
         current_quadrant = "top_left"
     }
+    
 }
-let direction = 0
-let current_pitch_in_degrees = 0
-let current_roll_in_degrees = 0
-let current_quadrant = ""
-let absolute_roll = 0
-let absolute_pitch = 0
-current_quadrant = "top_right"
-let main_sprite = game.createSprite(2, 4)
-let goal_sprite = game.createSprite(2, 0)
-basic.forever(function () {
-    current_roll_in_degrees = input.rotation(Rotation.Roll)
-    current_pitch_in_degrees = input.rotation(Rotation.Pitch)
-    determineQuadrant()
-    absolute_pitch = Math.abs(current_pitch_in_degrees)
-    absolute_roll = Math.abs(current_roll_in_degrees)
+
+function directionForQuadrant() {
+    
     direction = Math.atan(absolute_roll / absolute_pitch) * 57.2958
     if (current_quadrant == "top_right") {
-        direction = direction
+        direction = 90 - direction
     } else if (current_quadrant == "bottom_right") {
         direction = direction + 90
     } else if (current_quadrant == "bottom_left") {
@@ -34,12 +24,33 @@ basic.forever(function () {
     } else {
         direction = 270 + direction
     }
+    
+}
+
+let direction = 0
+let current_pitch_in_degrees = 0
+let current_roll_in_degrees = 0
+let current_quadrant = ""
+let absolute_pitch = 0
+let absolute_roll = 0
+current_quadrant = "top_right"
+let main_sprite = game.createSprite(2, 4)
+let goal_sprite = game.createSprite(0, 0)
+basic.forever(function on_forever() {
+    
+    current_roll_in_degrees = input.rotation(Rotation.Roll)
+    current_pitch_in_degrees = input.rotation(Rotation.Pitch)
+    determineQuadrant()
+    absolute_pitch = Math.abs(current_pitch_in_degrees)
+    absolute_roll = Math.abs(current_roll_in_degrees)
+    directionForQuadrant()
     main_sprite.set(LedSpriteProperty.Direction, direction)
     main_sprite.move(1)
 })
-basic.forever(function () {
+basic.forever(function on_forever2() {
+    
     basic.pause(800)
     goal_sprite.delete()
     basic.pause(800)
-    goal_sprite = game.createSprite(2, 0)
+    goal_sprite = game.createSprite(0, 0)
 })
